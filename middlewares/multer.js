077257bfdfec0888e24f534f49b33d1ef4ef2161
NImnaka <10899636@students.plymouth.ac.uk>
2025-05-04 20:38:42 +0530

    Update multer.js

--- middlewares/multer.js
+++ middlewares/multer.js
@@ -5,12 +5,10 @@ const cloudinary = require('../config/cloudinary');
 const storage = new CloudinaryStorage({
   cloudinary,
   params: {
-    folder: 'snapwear/products', // Cloudinary folder
+    folder: 'snapwear/products',
     allowed_formats: ['jpg', 'png', 'jpeg'],
-    transformation: [{ width: 800, height: 800, crop: "limit" }]
-  }
+    transformation: [{ width: 800, height: 800, crop: 'limit' }],
+  },
 });
 
-const upload = multer({ storage });
-
-module.exports = upload;
+module.exports = multer({ storage });
