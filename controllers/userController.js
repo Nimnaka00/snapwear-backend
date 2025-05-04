4ccb7e7375455dd0ceb5155af376727071ef5b67
NImnaka <10899636@students.plymouth.ac.uk>
2025-05-04 20:38:38 +0530

    Update userController.js

--- controllers/userController.js
+++ controllers/userController.js
@@ -4,7 +4,6 @@ exports.getUserProfile = async (req, res) => {
   try {
     const user = await User.findById(req.user.userId).select("-password");
     if (!user) return res.status(404).json({ message: "User not found" });
-
     res.json(user);
   } catch (err) {
     res.status(500).json({ error: err.message });
