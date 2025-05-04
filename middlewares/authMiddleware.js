15e062f60f32c131e2b1863bb2de2ead4a433748
NImnaka <10899636@students.plymouth.ac.uk>
2025-05-04 20:38:40 +0530

    Update authMiddleware.js

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -2,14 +2,11 @@ const jwt = require('jsonwebtoken');
 
 const authMiddleware = (req, res, next) => {
   const token = req.headers.authorization?.split(" ")[1];
-
-  if (!token) {
-    return res.status(401).json({ message: "Access denied. No token provided." });
-  }
+  if (!token) return res.status(401).json({ message: "Access denied. No token provided." });
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    req.user = decoded; // now you can access `req.user` in any protected route
+    req.user = decoded;
     next();
   } catch (err) {
     return res.status(400).json({ message: "Invalid or expired token." });
